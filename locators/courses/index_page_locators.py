fe86449a757a2b439ca9e335ff10013b291e6834
Davit Grigoryan <dawud.grigoryan@gmail.com>
2024-07-19 14:12:52 +0400

    changed course table id name

--- locators/courses/index_page_locators.py
+++ locators/courses/index_page_locators.py
@@ -6,6 +6,6 @@ class IndexPageLocators:
     ADD_NEW_COURSE_LINK = (By.CSS_SELECTOR, 'a[href="' + route_app('courses/add') + '"]')
     COURSE_NAME = (By.NAME, "name")
     SAVE_BUTTON = (By.ID, "add-new-course-btn")
-    COURSE_TABLE = (By.ID, 'es_automation_course-table')
+    COURSE_TABLE = (By.ID, 'es_automation_courses-table')
     DELETE_COURSE_BTN = (By.CLASS_NAME, 'delete_course_btn')
     DELETE_VERIFY_FIELD = (By.CLASS_NAME, "delete_verify_field")
